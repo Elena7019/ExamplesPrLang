51cddc21a2e0e4c5dedb06b6663b36ab0365588b
Elena <helenmai@yandex.ru>
2022-07-12 23:57:54 +0300

    Sravnenye chisel kvadrat2

---
+++ Example010ASravnenyeChisel/Program.cs
@@ -0,0 +1,8 @@
+﻿// Решение задачи Example010 вторым способом
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result=b*b;
+Console.Write((a==result)? "Yes":"No");
+
