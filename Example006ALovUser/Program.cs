28229c32bd6cea06360f0b51a725aa9359470d5f
Elena <helenmai@yandex.ru>
2022-07-12 17:05:08 +0300

    LovUserA

---
+++ Example006ALovUser/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача приветствия любимого пользователя, но имя может быть написано либо строчными, либо заглавными буквами
+
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.Write("Ура, это же Маша");
+}
+else
+{    
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
