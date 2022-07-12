55b9a44423b1fd5a91816ce8cd5f7aa28bb5d142
Elena <helenmai@yandex.ru>
2022-07-12 23:39:34 +0300

    Sravnenye chisel kvadrat

---
+++ Example010Sravnenye_chisel/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("a = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB == numberA * numberA)
+{
+   Console.WriteLine("Первое число является квадратом второго"); 
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
