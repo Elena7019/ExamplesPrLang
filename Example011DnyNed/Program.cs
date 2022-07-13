9e69ff1175c1bb0a2fbe04203a52da595741135d
Elena <helenmai@yandex.ru>
2022-07-13 17:14:45 +0300

    Nomer dnya nedeli

---
+++ Example011DnyNed/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Введите номер дня недели");
+string[] array = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Console.Write(array[Convert.ToInt32(Console.ReadLine()) - 1]);
