3fece26fe072a8d5cea01c49d6db9b70e620f70c
Elena <helenmai@yandex.ru>
2022-07-14 13:34:02 +0300

    Funktciya kvadrat chisla

---
+++ Example0012VychislKvadrat/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = 3;
+Console.WriteLine(Power(number));
+int Power(int currentNumber)
+{
+    return currentNumber * currentNumber;
+}
+/*/второй способ
+int number = 8;
+PowerProcedural(number);
+
+void PowerProcedural(int currentNumber)
+{
+Console.WriteLine(currentNumber * currentNumber);
+}*/
