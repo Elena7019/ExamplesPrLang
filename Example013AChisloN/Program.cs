523dba3cc7a52019fa16cfe2c021261dfb56407c
Elena <helenmai@yandex.ru>
2022-07-15 08:48:42 +0300

    Ryad chisel ot -N do N_2

---
+++ Example013AChisloN/Program.cs
@@ -0,0 +1,10 @@
+﻿//Второй способ решения задачи про ряд чисел от -N до N
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a=-N;
+int b=N; // [-N] = [a,b}]
+for(int i=a; i<=b; i++)
+{
+    Console.Write(i+" ");
+}
+Console.WriteLine();
