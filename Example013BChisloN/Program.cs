71325df09f9c090a5288e050f385062c3ea439ca
Elena <helenmai@yandex.ru>
2022-07-15 09:10:28 +0300

    Ryad chisel ot -N do N_3

---
+++ Example013BChisloN/Program.cs
@@ -0,0 +1,12 @@
+﻿//решение для положительных и отрицательных чисел (ряд чисел от -N до N)
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+int startnumber = -number;
+while(startnumber != (number + 1))
+{
+    Console.Write(startnumber + ",");
+    startnumber+=1;
+}
+Console.Write("\b.");
+Console.WriteLine();
