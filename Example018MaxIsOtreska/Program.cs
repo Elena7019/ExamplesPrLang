aeda1430dfacbb2584b77bc5e07fa606407a1b46
Elena <helenmai@yandex.ru>
2022-07-20 13:57:15 +0300

    Max Tsifra Chisla Is Otreska

---
+++ Example018MaxIsOtreska/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99]
+ //и показывает наибольшую цифру числа
+void Zadacha9()
+{
+Random rand = new Random();
+int number = rand.Next(10, 100);
+Console.WriteLine(number);
+int digitOnes = number%10;
+int digitTens = number/10;
+int max = digitTens;
+if(digitOnes > digitTens) max = digitOnes;
+Console.WriteLine("Наибольшая цифра числа " + max);
+}
+
+Zadacha9();
