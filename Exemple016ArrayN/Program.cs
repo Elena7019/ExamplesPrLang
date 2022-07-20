333e8fd34df787bd3eacc9906080061537b5358c
Elena <helenmai@yandex.ru>
2022-07-20 11:15:19 +0300

    FindN

---
+++ Exemple016ArrayN/Program.cs
@@ -0,0 +1,16 @@
+﻿// В массиве N найти элемент find
+int[] array = {12, 23, 34, 56, 678, 54, 43, 43, 341, 90};
+
+int n = array.Length;
+int find = 43;
+int index = 0;
+
+while(index < n )
+{
+    if(array[index] == find)
+    {
+    Console.WriteLine(index);
+    break;
+    }
+    index++;
+}
