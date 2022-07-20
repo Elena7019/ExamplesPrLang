f43fc71cf4d62dd73ae9992acba26ee29a617e63
Elena <helenmai@yandex.ru>
2022-07-20 18:23:27 +0300

    Vyvod chisla i udalenie tsifry

---
+++ Example019VyvodSUdaleniyem/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и далаяет среднюю цифру из него
+/*void Zadacha11()
+{
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+Console.WriteLine(number);
+int digitSot = number/100;
+int digitOnes = (number - 100*digitSot)%10;
+
+Console.WriteLine(digitSot + ""+ digitOnes);
+}
+
+Zadacha11();*/
+
+/*void Sposob2()
+{
+    Random rand = new Random();
+    int number = rand.Next(100, 1000);
+    Console.WriteLine(number);
+    int digitSot = number/100;
+    int digitOnes = number%10;
+    Console.WriteLine(digitSot*10 + digitOnes);
+}
+
+Sposob2();*/
+
+void Sposob3()
+{
+    string number = Convert.ToString(new Random().Next(100, 999));
+    int count = 0;
+    Console.WriteLine(number);
+    foreach(char c in number)
+    {
+        count++;
+        if(count !=2)
+        {
+            Console.Write(c);
+        }
+    }
+    Console.WriteLine();
+}
+
+Sposob3();
