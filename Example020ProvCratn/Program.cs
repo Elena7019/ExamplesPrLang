de31014903030cfc135bc5fa0f378d6d537c3250
Elena <helenmai@yandex.ru>
2022-07-21 15:54:33 +0300

    Provirka kratnosti

---
+++ Example020ProvCratn/Program.cs
@@ -0,0 +1,25 @@
+﻿//Ввод двух чисел, проверка, является ли второе число кратным первому, если нет - вывод остатка от деления
+void Zadacha12()
+{
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int res = number2 % number1;
+if (res > 0)
+{
+    Console.WriteLine("Второе число не кратно первому. Остаток от деления составляет ");
+    Console.WriteLine(res);  
+}
+else
+{
+    Console.WriteLine("Второе число кратно первому");
+}
+}
+Zadacha12();
+/*Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int res = number2 % number1;
+Console.WriteLine((number2%number1) == 0? $"Число {number2} кратно {number1}" : $"Остаток от деления: {number2%number1}");*/
